4b69f3be953b173e97807eab49b988f822cbce18
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-02 20:38:48 -0300

    Oculando la conexion de la BD en Variables de Entorno

--- src/config/db.js
+++ src/config/db.js
@@ -2,13 +2,10 @@ import mongoose from "mongoose";
 
 const conectarDB = async () => {
   try {
-    const connection = await mongoose.connect(
-      "mongodb+srv://tomidziurdzia:Walter960@cluster.aclfo.mongodb.net/projectsrepositories?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     const url = `${connection.connection.host}: connection.connection.port`;
     console.log(`MongoDB Conectado en: ${url}`);
   } catch (error) {
