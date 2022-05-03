9bef6f6096375bacd4808cede301ec0079bde3c2
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-03 17:50:11 -0300

    Creando el Modelo de Usuarios

---
+++ src/models/Usuario.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const usuarioSchema = mongoose.Schema(
+  {
+    nombre: {
+      type: String,
+      require: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      require: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      trim: true,
+      unique: true,
+    },
+    token: {
+      type: String,
+    },
+    confirmado: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Usuario = mongoose.model("Usuario", usuarioSchema);
+export default Usuario;
