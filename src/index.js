7e2068b11d569b275f57063f33f785ceee562a0a
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-03 18:01:36 -0300

    Routing de Usuarios

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import conectarDB from "./config/db.js";
+import usuarioRoutes from "./routes/usuarioRoutes.js";
 
 const app = express();
 
@@ -8,8 +9,10 @@ dotenv.config();
 
 conectarDB();
 
-const PORT = process.env.PORT || 4000;
+// Routing
+app.get("/api/usuarios", usuarioRoutes);
 
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
