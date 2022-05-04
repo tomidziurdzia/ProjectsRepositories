5e66bab13d44f0f5ca3fc63707c3a4d442d82c40
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-04 17:13:11 -0300

    Genere un ID para token unico, Cree el endpoint de autenticacion, valide la autenticacion, comrobe si el password es correcto y por ultimo agrege el Json Web Token

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
-import { registrar } from "../controllers/usuarioController.js";
+import { registrar, autenticar } from "../controllers/usuarioController.js";
 
 // Autenticacion, Registro y Confirmacion de Usuarios
 
 router.post("/", registrar); // Crea un nuevo usuario
+router.post("/login", autenticar);
 
 export default router;
