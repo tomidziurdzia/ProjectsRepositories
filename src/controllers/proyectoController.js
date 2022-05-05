0a240aec5f0e34479fdbf7bc52de32b88c271069
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-05 19:54:42 -0300

    Actualizando un Proyecto

--- src/controllers/proyectoController.js
+++ src/controllers/proyectoController.js
@@ -35,7 +35,33 @@ const obtenerProyecto = async (req, res) => {
   res.json(proyecto);
 };
 
-const editarProyecto = async (req, res) => {};
+const editarProyecto = async (req, res) => {
+  const { id } = req.params;
+
+  const proyecto = await Proyecto.findById(id);
+
+  if (!proyecto) {
+    const error = new Error("No encontrado");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Accion no valida");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  proyecto.nombre = req.body.nombre || proyecto.nombre;
+  proyecto.descripcion = req.body.descripcion || proyecto.descripcion;
+  proyecto.fechaEntrega = req.body.fechaEntrega || proyecto.fechaEntrega;
+  proyecto.cliente = req.body.cliente || proyecto.cliente;
+
+  try {
+    const proyectoAlmacenado = await proyecto.save();
+    res.json(proyectoAlmacenado);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const eliminarProyecto = async (req, res) => {};
 
