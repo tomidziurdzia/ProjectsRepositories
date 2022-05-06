4c637cf251bd7c43cd91e0c392563a6f8ba7191f
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-06 19:06:30 -0300

    Actualizando Tareas

--- src/controllers/tareaControler.js
+++ src/controllers/tareaControler.js
@@ -39,10 +39,35 @@ const obtenerTarea = async (req, res) => {
     return res.status(403).json({ msg: error.message });
   }
   res.json(tarea);
-  console.log(tarea);
 };
 
-const actualizarTarea = async (req, res) => {};
+const actualizarTarea = async (req, res) => {
+  const { id } = req.params;
+
+  const tarea = await Tarea.findById(id).populate("proyecto");
+
+  if (!tarea) {
+    const error = new Error("La tarea no fue encontrada");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Accion no valida");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  tarea.nombre = req.body.nombre || tarea.nombre;
+  tarea.descripcion = req.body.descripcion || tarea.descripcion;
+  tarea.prioridad = req.body.prioridad || tarea.prioridad;
+  tarea.fechaEntrega = req.body.fechaEntrega || tarea.fechaEntrega;
+
+  try {
+    const tareaAlmacenada = await tarea.save();
+    res.json(tareaAlmacenada);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const eliminarTarea = async (req, res) => {};
 
