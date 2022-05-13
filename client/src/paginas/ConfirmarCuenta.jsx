c7eef614c1a81381452d54f15ee1f5cd8e3c9ac4
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-13 16:58:20 -0300

    Termine de crear la pagina de nuevo usuario y confirmar cuenta. Con esto termino el diseño de todos los componentes publicos

--- client/src/paginas/ConfirmarCuenta.jsx
+++ client/src/paginas/ConfirmarCuenta.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const ConfirmarCuenta = () => {
-  return <div>ConfirmarCuenta</div>;
+  return (
+    <>
+      <h1 className="text-sky-600 font-black text-center text-4xl md:text-6xl">
+        Confirmar Cuenta
+      </h1>
+    </>
+  );
 };
 
 export default ConfirmarCuenta;
--- client/src/paginas/NuevoPassword.jsx
+++ client/src/paginas/NuevoPassword.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const NuevoPassword = () => {
-  return <div>NuevoPassword</div>;
+  return (
+    <>
+      <h1 className="text-sky-600 font-black text-center text-4xl md:text-6xl">
+        Reestablece tu contraseña y no pierdas acceso a tus{" "}
+        <span className="text-slate-700">proyectos</span>
+      </h1>
+      <form className="my-10 bg-white shadow rounded-lg p-10">
+        <div className="my-5">
+          <label
+            htmlFor="password"
+            className="text-gray-600 block text-xl font-bold"
+          >
+            Nueva Contraseña
+          </label>
+          <input
+            id="password"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            type="password"
+            placeholder="Nueva contraseña"
+          />
+        </div>
+        <div className="my-5">
+          <label
+            htmlFor="repetir-password"
+            className="text-gray-600 block text-xl font-bold"
+          >
+            Repetir Contraseña
+          </label>
+          <input
+            id="repetir-password"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            type="password"
+            placeholder="Repetir contraseña"
+          />
+        </div>
+        <input
+          type="submit"
+          value="Reestablecer Contraseña"
+          className="bg-sky-600 w-full py-3 text-white font-bold rounded hover:cursor-pointer hover:bg-sky-700 transition-colors"
+        />
+      </form>
+      <nav className="lg:flex lg:justify-between">
+        <Link className="block text-center my-5 text-slate-500 text-sm" to="/">
+          ¿Ya tienes una cuenta? Inicia Sesión
+        </Link>
+        <Link
+          className="block text-center my-5 text-slate-500 text-sm"
+          to="/olvide-password"
+        >
+          Olvide mi password
+        </Link>
+      </nav>
+    </>
+  );
 };
 
 export default NuevoPassword;
