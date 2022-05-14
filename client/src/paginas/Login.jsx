26e588c7d3d16548a2663e156524f24b9ffb193f
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-14 17:15:21 -0300

    Validando la Autenticacion

--- client/src/paginas/Login.jsx
+++ client/src/paginas/Login.jsx
@@ -1,13 +1,40 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Alerta from "../components/Alerta";
+import clienteAxios from "../config/clienteAxios";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [alerta, setAlerta] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if ([email, password].includes("")) {
+      setAlerta({
+        msg: "Todos los campos son obligatorios",
+        error: true,
+      });
+      return;
+    }
+  };
+
+  const { msg } = alerta;
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-center text-4xl md:text-6xl">
         Inicia sesión y administra tus{" "}
         <span className="text-slate-700">proyectos</span>
       </h1>
-      <form className="my-10 bg-white shadow rounded-lg p-10">
+
+      {msg && <Alerta alerta={alerta} />}
+
+      <form
+        className="my-10 bg-white shadow rounded-lg p-10"
+        onSubmit={handleSubmit}
+      >
         <div className="my-5">
           <label
             htmlFor="email"
@@ -20,6 +47,8 @@ const Login = () => {
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
             type="email"
             placeholder="Email de registro"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="my-5">
@@ -34,6 +63,8 @@ const Login = () => {
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
             type="password"
             placeholder="Password de registro"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <input
