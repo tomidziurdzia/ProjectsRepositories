990f1bcf394f3cf6893c25179839378f82953a4e
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-14 19:45:07 -0300

    Creando un Hook para el Context de Proyectos

--- client/src/context/ProyectosProvider.jsx
+++ client/src/context/ProyectosProvider.jsx
@@ -4,8 +4,12 @@ import clienteAxios from "../config/clienteAxios";
 const ProyectosContext = createContext();
 
 const ProyectosProvider = ({ children }) => {
+  const [proyectos, setProyectos] = useState([]);
+
   return (
-    <ProyectosContext.Provider value={{}}>{children}</ProyectosContext.Provider>
+    <ProyectosContext.Provider value={{ proyectos }}>
+      {children}
+    </ProyectosContext.Provider>
   );
 };
 
--- client/src/paginas/Proyectos.jsx
+++ client/src/paginas/Proyectos.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import useProyectos from "../hooks/useProyectos";
 
 const Proyectos = () => {
+  const { proyectos } = useProyectos();
+
   return (
     <>
       <h1 className="text-4xl font-black">Proyectos</h1>
