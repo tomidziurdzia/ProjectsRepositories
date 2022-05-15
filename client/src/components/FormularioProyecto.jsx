7b44eff1e4dfa4b806f93be10307b1101cb47a40
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-15 12:54:52 -0300

    Llenando el Formulario con la Informacion del Proyecto

--- client/src/components/FormularioProyecto.jsx
+++ client/src/components/FormularioProyecto.jsx
@@ -1,14 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import useProyectos from "../hooks/useProyectos";
 import Alerta from "./Alerta";
 
 const FormularioProyecto = () => {
+  const [id, setId] = useState(null);
   const [nombre, setNombre] = useState("");
   const [descripcion, setDescripcion] = useState("");
   const [fechaEntrega, setFechaEntrega] = useState("");
   const [cliente, setCliente] = useState("");
 
-  const { mostrarAlerta, alerta, submitProyecto } = useProyectos();
+  const params = useParams();
+
+  const { mostrarAlerta, alerta, submitProyecto, proyecto } = useProyectos();
+
+  useEffect(() => {
+    if (params.id) {
+      setId(proyecto._id);
+      setNombre(proyecto.nombre);
+      setDescripcion(proyecto.descripcion);
+      setFechaEntrega(proyecto.fechaEntrega?.split("T")[0]);
+      setCliente(proyecto.cliente);
+    }
+  }, [params]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -96,7 +110,7 @@ const FormularioProyecto = () => {
       <input
         className="bg-sky-600 w-full p-3 font-bold text-white rounded cursor-pointer hover:bg-sky-700 transition-colors"
         type="submit"
-        value="Crear Proyecto"
+        value={id ? "Editar Proyecto" : "Crear Proyecto"}
       />
     </form>
   );
