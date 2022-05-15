02cbaab51c3fb2740c9bb31f6d688b0d43ac3e24
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-15 13:09:23 -0300

    Actualizar o Crear Proyecto segun sea necesario

--- client/src/components/FormularioProyecto.jsx
+++ client/src/components/FormularioProyecto.jsx
@@ -36,8 +36,9 @@ const FormularioProyecto = () => {
     }
 
     // Pasar los datos hacia el provider
-    await submitProyecto({ nombre, descripcion, fechaEntrega, cliente });
+    await submitProyecto({ id, nombre, descripcion, fechaEntrega, cliente });
 
+    setId(null);
     setNombre("");
     setDescripcion("");
     setFechaEntrega("");
