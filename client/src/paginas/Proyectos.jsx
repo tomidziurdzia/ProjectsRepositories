355561db41d27e16577ea6f3b547ca2892f21650
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-15 11:40:12 -0300

    Obteniendo los Proyectos del usuario

--- client/src/paginas/Proyectos.jsx
+++ client/src/paginas/Proyectos.jsx
@@ -2,11 +2,18 @@ import useProyectos from "../hooks/useProyectos";
 
 const Proyectos = () => {
   const { proyectos } = useProyectos();
+  console.log(proyectos);
 
   return (
     <>
       <h1 className="text-4xl font-black">Proyectos</h1>
-      <div></div>
+      <div className="bg-white shadow mt-10 rounded-lg">
+        {proyectos.length ? (
+          <p>si hay</p>
+        ) : (
+          <p className="py-5 text-center text-gray600">No hay proyectos aún</p>
+        )}
+      </div>
     </>
   );
 };
