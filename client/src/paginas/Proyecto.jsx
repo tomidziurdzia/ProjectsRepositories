2a6fa2fb3ba6ac02cc64fb8953dfa94dbf48fbde
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-16 16:35:57 -0300

    Creando boton de añadir nueva tarea

--- client/src/paginas/Proyecto.jsx
+++ client/src/paginas/Proyecto.jsx
@@ -18,9 +18,33 @@ const Proyecto = () => {
   if (cargando) return "Cargando...";
 
   return (
-    <div className="flex justify-between">
-      <h1 className="font-black text-4xl">{nombre}</h1>
-      <div className="flex items-center gap-2 text-gray-400 hover:text-gray-700">
+    <>
+      <div className="flex justify-between">
+        <h1 className="font-black text-4xl">{nombre}</h1>
+        <div className="flex items-center gap-2 text-gray-400 hover:text-gray-700">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+            />
+          </svg>
+          <Link to={`/proyectos/editar/${params.id}`} className="font-bold">
+            Editar
+          </Link>
+        </div>
+      </div>
+      <button
+        type="button"
+        className="flex gap-2 items-center justify-center text-sm px-5 py-3 w-full md:w-auto mt-5 rounded-lg font-bold bg-sky-400 text-white text-center"
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-6 w-6"
@@ -32,14 +56,12 @@ const Proyecto = () => {
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+            d="M12 4v16m8-8H4"
           />
         </svg>
-        <Link to={`/proyectos/editar/${params.id}`} className="font-bold">
-          Editar
-        </Link>
-      </div>
-    </div>
+        Nueva Tarea
+      </button>
+    </>
   );
 };
 
