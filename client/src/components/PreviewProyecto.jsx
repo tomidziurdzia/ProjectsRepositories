93f2b72b40ac3ce9047a746a19bd8cc6c40d985f
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-17 20:18:44 -0300

    Mostrar la leyenda Colaborador cuando no son proyectos propios

--- client/src/components/PreviewProyecto.jsx
+++ client/src/components/PreviewProyecto.jsx
@@ -1,14 +1,29 @@
 import { Link } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 const PreviewProyecto = ({ proyecto }) => {
-  const { nombre, _id, cliente } = proyecto;
-  return (
-    <div className="border-b p-5 flex">
-      <p className="flex-1">
-        {nombre}
+  const { auth } = useAuth();
 
-        <span className="text-sm text-gray-500"> {cliente}</span>
-      </p>
+  const { nombre, _id, cliente, creador } = proyecto;
+
+  return (
+    <div className="border-b p-5 flex justify-between">
+      <div className="flex items-center gap-2">
+        <p className="flex-1">
+          {nombre}
+
+          <span className="text-sm text-gray-500">
+            {""} {cliente}
+          </span>
+        </p>
+
+        {auth._id !== creador && (
+          <p className="p-1 text-xs rounded-lg text-white bg-green-500 font-bold">
+            Colaborador
+          </p>
+        )}
+      </div>
+
       <Link
         to={`${_id}`}
         className="text-gray-500 hover:text-gray-800 text-sm font-bold transition-colors"
