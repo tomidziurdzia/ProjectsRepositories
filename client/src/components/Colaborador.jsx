95817b68db340092cf7db0c4c47c2a1b62967919
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-17 18:12:44 -0300

    Mostrando la Infomracion de Colaboradores

--- client/src/components/Colaborador.jsx
+++ client/src/components/Colaborador.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const Colaborador = () => {
-  return <div>Colaborador</div>;
+const Colaborador = ({ colaborador }) => {
+  const { email, nombre } = colaborador;
+  return (
+    <div className="border-b p-5 flex justify-between items-center">
+      <div>
+        <p>{nombre}</p>
+        <p className="text-sm text-gray-700">{email}</p>
+      </div>
+      <div>
+        <button
+          type="button"
+          className="bg-red-600 px-4 py-3 text-white font-bold text-sm rounded-lg"
+        >
+          Eliminar
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Colaborador;
