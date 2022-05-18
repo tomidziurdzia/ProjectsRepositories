2edf94807df03fb9a8c084e5cca40c2b2e237dc7
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-18 10:53:21 -0300

    Añadiendo Buscador de Proyectos

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
+import useProyectos from "../hooks/useProyectos";
+import Busqueda from "./Busqueda";
 
 const Header = () => {
+  const { handleBuscador } = useProyectos();
+
   return (
     <header className="px-4 py-5 bg-white border-b">
       <div className="md:flex md:justify-between">
@@ -9,7 +13,11 @@ const Header = () => {
         </h2>
 
         <div className="flex flex-col md:flex-row items-center gap-4">
-          <button type="button" className="font-bold text-gray-400">
+          <button
+            onClick={handleBuscador}
+            type="button"
+            className="font-bold text-gray-400"
+          >
             Buscar Proyecto
           </button>
           <Link to="/proyectos" className="font-bold text-gray-400">
@@ -21,6 +29,7 @@ const Header = () => {
           >
             Cerrar Sesion
           </button>
+          <Busqueda />
         </div>
       </div>
     </header>
