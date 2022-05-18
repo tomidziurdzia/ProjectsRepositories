ffceb1579377024aad3eae2b6c744737792c985b
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-18 13:28:17 -0300

    Solucionar Carga de Proyectos al INICIAR SESION

--- client/src/context/ProyectosProvider.jsx
+++ client/src/context/ProyectosProvider.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, createContext } from "react";
 import clienteAxios from "../config/clienteAxios";
 import { useNavigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 import io from "socket.io-client";
 
 let socket;
@@ -21,6 +22,8 @@ const ProyectosProvider = ({ children }) => {
   const [buscador, setBuscador] = useState(false);
 
   const navigate = useNavigate();
+
+  const { auth } = useAuth();
 
   useEffect(() => {
     const obtenerProyectos = async () => {
@@ -43,7 +46,7 @@ const ProyectosProvider = ({ children }) => {
     };
 
     obtenerProyectos();
-  }, []);
+  }, [auth]);
 
   useEffect(() => {
     socket = io(import.meta.env.VITE_BACKEND_URL);
